5229f84d23d6485d9167e881faae1e344b5afd25
R M <friedbroccolis@gmail.com>
2021-09-15 21:48:47 +0300

    Edit pom dependencies and .gitignore file

--- README.md
+++
@@ -1,2 +0,0 @@
-# spring-boot-email-sender
-
--- src/main/java/com/example/sociopathy/controller/RegistrationController.java
+++ src/main/java/com/example/sociopathy/controller/RegistrationController.java
@@ -4,8 +4,10 @@ import com.example.sociopathy.model.User;
 import com.example.sociopathy.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
 
 @Controller
 public class RegistrationController {
